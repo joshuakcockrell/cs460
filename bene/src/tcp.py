c2a36680f404bd2d660408408470b3c80715c0d2
joshuakcockrell <joshuakcockrell@gmail.com>
2018-02-16 13:06:43 -0700

    Files

---
+++ bene/src/tcp.py
@@ -0,0 +1,194 @@
+from .buffer import SendBuffer, ReceiveBuffer
+from .connection import Connection
+from .sim import Sim
+from .tcppacket import TCPPacket
+
+
+class TCP(Connection):
+    """ A TCP connection between two hosts."""
+
+    def __init__(self, transport, source_address, source_port,
+                 destination_address, destination_port, app=None, window=1000,drop=[]):
+        Connection.__init__(self, transport, source_address, source_port,
+                            destination_address, destination_port, app)
+
+        # -- Sender functionality
+
+        # send window; represents the total number of bytes that may
+        # be outstanding at one time
+        self.window = window
+        self.cur_window = self.window
+        # send buffer
+        self.send_buffer = SendBuffer()
+        # maximum segment size, in bytes
+        # self.mss = 1000
+        self.mss = 2
+        # largest sequence number that has been ACKed so far; represents
+        # the next sequence number the client expects to receive
+        self.sequence = 0
+        # plot sequence numbers
+        self.plot_sequence_header()
+        # packets to drop
+        self.drop = drop
+        self.dropped = []
+        # retransmission timer
+        self.timer = None
+        # timeout duration in seconds
+        self.timeout = 1
+
+        # -- Receiver functionality
+
+        # receive buffer
+        self.receive_buffer = ReceiveBuffer()
+        # ack number to send; represents the largest in-order sequence
+        # number not yet received
+        self.ack = 0
+
+    def trace(self, message):
+        """ Print debugging messages. """
+        Sim.trace("TCP", message)
+
+    def plot_sequence_header(self):
+        if self.node.hostname =='n1':
+            Sim.plot('sequence.csv','Time,Sequence Number,Event\n')
+
+    def plot_sequence(self,sequence,event):
+        if self.node.hostname =='n1':
+            Sim.plot('sequence.csv','%s,%s,%s\n' % (Sim.scheduler.current_time(),sequence,event))
+
+    def receive_packet(self, packet):
+        print ''
+        print 'RECEIVE DATA'
+        print 'length',packet.length
+        print 'ack',packet.ack_number
+        print 'seq',packet.sequence
+
+        """ Receive a packet from the network layer. """
+        # if packet.ack_number > 0:
+            # handle ACK
+        self.handle_ack(packet)
+        if packet.length > 0:
+            # handle data
+            self.handle_data(packet)
+
+    ''' Sender '''
+
+
+    def send_on_ack(self):
+
+        if not self.send_buffer.available():
+            return
+
+        print ''
+        print 'SEND DATA'
+        print 'base_seq',self.send_buffer.base_seq
+        print 'next_seq',self.send_buffer.next_seq
+        print 'last_seq',self.send_buffer.last_seq
+        print 'outstanding',self.send_buffer.outstanding()
+
+        while self.cur_window > 0:
+            self.cur_window -= 1
+
+            print 'GET data'
+            next_data = self.send_buffer.get(self.mss)
+            print next_data
+
+            self.send_packet(next_data[0], next_data[1])
+            self.timer = Sim.scheduler.add(delay=self.timeout, event='retransmit', handler=self.retransmit)
+
+    def send(self, data):
+        """ Send data on the connection. Called by the application. This
+            code currently sends all data immediately. """
+
+        print 'Load into buffer..'
+        print data
+        self.send_buffer.put(data)
+
+        self.send_on_ack()
+
+    def send_packet(self, data, sequence):
+        packet = TCPPacket(source_address=self.source_address,
+                           source_port=self.source_port,
+                           destination_address=self.destination_address,
+                           destination_port=self.destination_port,
+                           body=data,
+                           sequence=sequence, ack_number=self.ack)
+
+        if sequence in self.drop and not sequence in self.dropped:
+            self.dropped.append(sequence)
+            self.plot_sequence(sequence,'drop')
+            self.trace("%s (%d) dropping TCP segment to %d for %d" % (
+                self.node.hostname, self.source_address, self.destination_address, packet.sequence))
+            return
+
+        # send the packet
+        self.plot_sequence(sequence,'send')
+        self.trace("%s (%d) sending TCP segment to %d for %d" % (
+            self.node.hostname, self.source_address, self.destination_address, packet.sequence))
+        self.transport.send_packet(packet)
+
+        # set a timer
+        if not self.timer:
+            self.timer = Sim.scheduler.add(delay=self.timeout, event='retransmit', handler=self.retransmit)
+
+    def handle_ack(self, packet):
+        """ Handle an incoming ACK. """
+        self.plot_sequence(packet.ack_number - 1000,'ack')
+        self.trace("%s (%d) received ACK from %d for %d" % (
+            self.node.hostname, packet.destination_address, packet.source_address, packet.ack_number))
+        # self.cancel_timer() # TODO FIX
+
+        self.send_buffer.slide(packet.ack_number)
+        self.cur_window += 1
+        self.send_on_ack()
+
+    def retransmit(self, event):
+        """ Retransmit data. """
+        self.trace("%s (%d) retransmission timer fired" % (self.node.hostname, self.source_address))
+
+    def cancel_timer(self):
+        """ Cancel the timer. """
+        if not self.timer:
+            return
+        Sim.scheduler.cancel(self.timer)
+        self.timer = None
+
+    ''' Receiver '''
+
+    def handle_data(self, packet):
+        """ Handle incoming data. This code currently gives all data to
+            the application, regardless of whether it is in order, and sends
+            an ACK."""
+        self.trace("%s (%d) received TCP segment from %d for %d" % (
+            self.node.hostname, packet.destination_address, packet.source_address, packet.sequence))
+
+
+        self.receive_buffer.put(packet.body, packet.sequence)
+
+
+        self.app.receive_data(packet.body) # send to app
+        print 'handle_data'
+        # print len(packet.body)
+        print packet.sequence
+        # print packet.ack_number
+
+        self.ack = packet.sequence # highest consec weve seen
+        self.send_ack()
+
+    def send_ack(self):
+        """ Send an ack. """
+        packet = TCPPacket(source_address=self.source_address,
+                           source_port=self.source_port,
+                           destination_address=self.destination_address,
+                           destination_port=self.destination_port,
+                           sequence=self.sequence, ack_number=self.ack)
+        # send the packet
+        self.trace("%s (%d) sending TCP ACK to %d for %d" % (
+            self.node.hostname, self.source_address, self.destination_address, packet.ack_number))
+        self.transport.send_packet(packet)
+
+
+
+
+
+
